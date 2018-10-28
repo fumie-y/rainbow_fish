58603f8375d949e64939500064398f7c74627317
矢田崎史恵 <fu19860308@gmail.com>
2018-10-28 23:21:50 +0900

    homeとusersのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+ 
+  post 'users/:id/update' => 'users#update'
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/create' =>  'users#create'
+  get 'signup' => 'users#new'
+  get 'users/index' => 'users#index'
+  get 'users/:id' => 'users#show'
+  get 'users/:id/likes_photo' => 'users#likes_photo'
+  post 'login' => 'users#login'
+  post 'logout' => 'users#logout'
+  get 'login' => 'users#login_form'
+  get 'users/destroy' => 'users#destroy'
+  
+  get '/' => 'home#top'
+  get 'about' => 'home#about'
+  
+  #homeコントローラーに入れるべきか新たにコントローラー作成するか保留中
+  get 'top_controller' =>
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
