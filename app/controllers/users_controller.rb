ba50ee5dad95748d9af540ee829122fcf7511edd
矢田崎史恵 <fu19860308@gmail.com>
2018-10-29 00:11:05 +0900

    初めてなので全てのファイルをコミットしました

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def likes_photo
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def login_form
+  end
+
+  def login
+  end
+
+  def logout
+  end
+
+  def destory
+  end
+  
+end
