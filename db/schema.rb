1b4a11325d2ab29315c349ac6b78fd8fe63544fc
矢田崎史恵 <fu19860308@gmail.com>
2018-10-29 22:15:22 +0900

    修正　ご指摘箇所修正、削除しました

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2018_10_28_075500) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "password"
+    t.string "name", null: false
+    t.string "password", null: false
     t.binary "profile_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
