c144e55b900e02f4291466bccc331e4e638d14d9
矢田崎史恵 <fu19860308@gmail.com>
2018-11-01 23:56:07 +0900

    userのindexとshowを追加しました。（indexはNoMethodErrorが出ています）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,35 @@
 class UsersController < ApplicationController
   def index
+  	@users = User.all
   end
 
   def show
+  	@user = User.find_by(id: params[:id])
   end
 
   def likes_photo
   end
 
   def new
-  end
-
-  def edit
+  	@user = User.new
   end
 
   def create
+  	@user = User.new(
+  		name: params[:name], 
+  		password: params[:password],
+  		profile_image: "default_user.jpg"
+  	)
+  	if @user.save
+  		session[:user_id] = @user.id
+  		flash[:notice] = 'ユーザー登録が完了しました'
+  		redirect_to = ("/users/#{@user.id}")
+  	else
+  		render = ("/users/new")
+  	end
+  end
+
+  def edit
   end
 
   def update
