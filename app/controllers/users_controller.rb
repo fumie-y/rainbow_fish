e6e193e00a1961726362c6f187c307bff9e88d5b
矢田崎史恵 <fu19860308@gmail.com>
2018-11-02 23:22:04 +0900

    indexとshow関連のファイルを更新しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,32 @@
 class UsersController < ApplicationController
   def index
-  	@users = User.all
+    @users = User.all
   end
 
   def show
-  	@user = User.find_by(id: params[:id])
+    @user = User.find(params[:id])
   end
 
   def likes_photo
   end
 
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def create
-  	@user = User.new(
-  		name: params[:name], 
-  		password: params[:password],
-  		profile_image: "default_user.jpg"
-  	)
-  	if @user.save
-  		session[:user_id] = @user.id
-  		flash[:notice] = 'ユーザー登録が完了しました'
-  		redirect_to = ("/users/#{@user.id}")
-  	else
-  		render = ("/users/new")
-  	end
+    @user = User.new(
+      name: params[:name], 
+      password: params[:password],
+      profile_image: "default_user.jpg"
+    )
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = 'ユーザー登録が完了しました'
+      redirect_to = ("/users/#{@user.id}")
+    else
+      render = ("/users/new")
+    end
   end
 
   def edit
