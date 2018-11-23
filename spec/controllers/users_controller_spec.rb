2b327200150a6844649b2c37027be5f854024376
矢田崎史恵 <fu19860308@gmail.com>
2018-11-24 08:08:47 +0900

    users_controller_spec.rb修正しました。

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,10 +14,3 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 end
-
-# describe "GET #new" do
-#     it "returns http success" do
-#       get :new
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
