49d315f780bf654d2e2571f1f24fb51eb40bfced
矢田崎史恵 <fu19860308@gmail.com>
2018-12-13 23:11:18 +0900

    photo_new更新できました。（画像だけです）

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
   validates :user_id, presence: true
-  mount_uploader :image, ImgUploader
+  mount_uploader :image, NewPhotoImageUploader
 
   has_many :tags
   has_many :likes
