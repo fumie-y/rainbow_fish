018c73c111719142fc3b50dc16e44beba4bc3596
矢田崎史恵 <fu19860308@gmail.com>
2018-12-25 22:32:56 +0900

    photo_new_current_user更新

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,5 @@
 class PhotosController < ApplicationController
+  before_action :authenticate_user, {only: [:new, :create, :edit, :update, :destroy]}
   def index
     @photos = Photo.all.order(created_at: :desc)
   end
