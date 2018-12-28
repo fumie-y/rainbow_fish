30468829591a189f2e48103bf00efaf48c62a202
矢田崎史恵 <fu19860308@gmail.com>
2018-12-28 21:30:05 +0900

    create_like2更新（photo#showの６４行目エラーが出ます）

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_12_17_145305) do
-
+ActiveRecord::Schema.define(version: 2018_12_28_111633) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -40,7 +38,7 @@ ActiveRecord::Schema.define(version: 2018_12_17_145305) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.binary "profile_image"
+    t.string "profile_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
