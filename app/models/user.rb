fa5965f80bacfc78bf85ddc00891b5e0cec21e7e
矢田崎史恵 <fu19860308@gmail.com>
2018-12-29 01:07:46 +0900

    user_edit2更新（user.rb修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true, length: {maximum: 50}
   validates :password, presence: true, length: {maximum: 50}, allow_nil: true
-   validates :password, presence: true, allow_nil: true
+  
   mount_uploader :profile_image, NewPhotoImageUploader
 
   has_many :photos #dependent: :destroy(後で表示)
