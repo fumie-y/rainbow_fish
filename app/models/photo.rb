9491cae2f14dc48b8c1e99c6fdd518b530aa2919
前川晃一郎 <qian41119122@gmail.com>
2019-01-09 21:50:22 +0900

    Merge branch 'master' of https://github.com/fumie-y/rainbow_fish into develop/top,about
    # Conflicts:
    #	Gemfile.lock

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,8 +3,9 @@ class Photo < ApplicationRecord
 
   mount_uploader :image, NewPhotoImageUploader
 
-
   has_many :tags
+  accepts_nested_attributes_for :tags
+
   has_many :likes
   belongs_to :user
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,8 @@
 class Tag < ApplicationRecord
   belongs_to :photo
+
+  def update_tag(tag_attribute)
+    self.name = tag_attribute.first['name']
+  end
 end
+
