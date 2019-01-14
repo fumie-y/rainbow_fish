d24f454c46be132c596f4e098c609093185d5d93
矢田崎史恵 <fu19860308@gmail.com>
2019-01-14 22:37:14 +0900

    issues3更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, presence: true, uniqueness: true, length: {maximum: 20}, on: :create
+  validates :name, presence: true, uniqueness: true, length: { in: 1..20 }
   validates :password, presence: true, length: {maximum: 20}, on: :create
-  validates :profile_image, presence: true, on: :create
   
   mount_uploader :profile_image, NewPhotoImageUploader
 
